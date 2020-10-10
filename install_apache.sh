e0c3365e49ea0f300bf9d2f649df999fcf435d84
vadimkholodilo <vadimkholodilo@gmail.com>
2020-10-10 21:17:27 +0300

    Apache installation script will change port Apache will be listening on if 80 is already taken

--- install_apache.sh
+++ install_apache.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
+
+# determining a port which Apache can listen on
+# Apache can be installed with NGINX, so to make everything working properly, we need to make sure that Apache will use a port which is not already used
+# ports. Feel free to modify this list according to your needs
+ports=(80, 8000, 8080)
+port=-1 # If after for loop this variable is still -1, it means that all ports in the list are busy
+for p in $ports[@]
+do
+lsof -i:$p > /dev/null
+if [ $? -eq 0 ]; then
+port=p
+fi
+done
+if [ $port -eq -1 ]; then
+echo "All ports are already taken"
+exit 1
+fi
+echo "Apache will be on port $port"
 apt install apache2 -y
+
+# If the port is different from 80, change it in /etc/apache2/ports.conf
+if [ $port -ne 80 ]; then
+sed -i "s/80/$port/g" /etc/apache2/ports.conf
+fi
+
+echo "Restarting Apache"
+systemctl restart apache2
+echo "Enabling Apache on boot"
+systemctl enable apache2
