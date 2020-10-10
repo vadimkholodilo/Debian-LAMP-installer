caa9bdbed3056b0cec34c3afca0840fac092dc17
vadimkholodilo <vadimkholodilo@gmail.com>
2020-10-10 10:47:30 +0300

    Required php extensions for most CMS were added

--- install_php.sh
+++ install_php.sh
@@ -5,7 +5,7 @@ add-apt-repository ppa:ondrej/php
 echo "Updating package indexes"
 apt update
 echo "Installing php 7.4"
-apt install php7.4
+apt install -y php7.4 php7.4-mysql php7.4-curl php7.4-json php7.4-mbstring php7.4-xml php7.4-zip php7.4-gd php7.4-soap php7.4-tokenizer
 echo "Enabling php module for apache"
 a2enmod php7.4
 echo "Restarting apache"
